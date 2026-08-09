518a02efec8e4444212b389cbfe3b026dab3e4ac
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-08-09 18:29:29 +0530

    Clearing Basic

---
+++ JS_DSA/map.js
@@ -0,0 +1,27 @@
+const number=[1,2,3,4,5,6,7,8,9,10];
+
+
+const result=number.map(num=>num);
+
+const  result1=number.map(num=>num*2);
+
+const result2=number.map(num=>num+1);
+
+const arr=['ritik','kumar','singhyadavkumar','ritik kumar singh','ritik kumar singhyadav'];
+const result4=arr.map(name=>name.length)
+
+const result5=number.map(num=>{
+    if(num%2==0)
+    {
+        return num*2;
+    }
+    return num;
+})
+
+
+
+// console.log(result);
+// console.log(result1);
+// console.log(result2);
+// console.log(result4);
+console.log(result5);
